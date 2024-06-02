f132c8008a3496778b954d0b7cd8dd8da72900ce
pavanpj123 <Pavanjadhav00001@gmail.com>
2024-06-02 11:26:31 -0700

    post Module

--- BlogApplication/src/main/java/com/BlogApplication/start/Entities/Category.java
+++ BlogApplication/src/main/java/com/BlogApplication/start/Entities/Category.java
@@ -1,10 +1,16 @@
 package com.BlogApplication.start.Entities;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name="categories")
 public final class Category {
-    
+     
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer categoryId;
@@ -22,7 +28,10 @@ public final class Category {
 	
 	@Column(name="descripation")
 	private String categoryDescription;
-
+    
+	@OneToMany(mappedBy = "category",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private Set<Post> posts=new LinkedHashSet<>();
+	
 	public Integer getCategoryId() {
 		return categoryId;
 	}
@@ -46,4 +55,5 @@ public final class Category {
 	public void setCategoryDescription(String categoryDescription) {
 		this.categoryDescription = categoryDescription;
 	}
+	
 }
